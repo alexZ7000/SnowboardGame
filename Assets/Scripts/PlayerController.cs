2f0d811914c6d1fc09d04c885bceb8561c47806e
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-02-17 12:44:19 -0300

    adicionando controles

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 1f;
+    private Rigidbody2D _rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rb2d.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rb2d.AddTorque(-torqueAmount);
+        }
+    }
+}
